b63fe3cba56213117936ceec9e43a3229d4fef62
korsarskiy <korsinsk@gmail.com>
2024-11-23 21:44:21 +0300

    Создал React, подготовил к работе

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('#textInput').on('input', function() {
-        var charCount = $(this).val().length; // Получаем длину текста
-        $('#charCount').text(charCount); // Обновляем счетчик символов
-    });
-});
