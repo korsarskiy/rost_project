da64f06bbf7ac34dc42c358955a4efb0961dfda7
korsarskiy <korsinsk@gmail.com>
2024-11-30 11:26:10 +0300

    Создал форму добавления постов

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import "./styles/app.css";
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 
 function App() {
   const [posts, setPosts] = useState([
-    {id: 1, title: 'Rost', body: 'Description'},
-    {id: 12, title: 'Rosdt', body: 'Description'},
-    {id: 11, title: 'Rostasdasd', body: 'Description'},
+    { id: 1, title: 'ХЕллоу', body: 'dikfhijifs'},
+    { id: 12, title: 'ХЕлasdлоу', body: 'dikfhijifs'},
+    { id: 14, title: 'ХЕлasdasdasdлоу', body: 'dikfhijifs'},
+    { id: 14, title: 'ХЕлasdasdasdлоу', body: 'dikfhijifs'}
+
   ])
 
 
@@ -19,10 +24,12 @@ function App() {
     <div className="App">
       <Counter/>
       <ClassCounter/>
-      <h1>Посты</h1>
-      {posts.map(post =>
-        <PostItem post={post} key={post.id}/>
-      )}
+      <form>
+        <MyInput type="text" placeholder="Название поста"/>
+        <MyInput type="text" placeholder="Название поста"/>
+        <MyButton disabled> Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title="Список постов 1"/>
     </div>
   );
 }
